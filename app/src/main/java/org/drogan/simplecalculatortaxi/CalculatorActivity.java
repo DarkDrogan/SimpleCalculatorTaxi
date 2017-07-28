92726ef566ad307397fae1ee08bfabe3e3817b04
darkdrogan <vedmag@bk.ru>
2017-07-28 17:47:21 +0300

    Save cost fuel and expense

--- app/src/main/java/org/drogan/simplecalculatortaxi/CalculatorActivity.java
+++ app/src/main/java/org/drogan/simplecalculatortaxi/CalculatorActivity.java
@@ -1,12 +1,18 @@
 package org.drogan.simplecalculatortaxi;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXPENSE_GASOLINE = "EXPENSE_GASOLINE";
+    public static final String COST_GASOLINE = "COST_GASOLINE";
 
     private double aFuel;
     private double aMoney;
@@ -18,51 +24,91 @@ public class CalculatorActivity extends AppCompatActivity {
     private EditText distanseET;
     private TextView incomeTV;
     private TextView clearIncomeTV;
+    Button saveInSQlButton, calculateButton;
+    SharedPreferences sPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-    }
 
-    public void cancelInfo(View view){
-        //NOP
-        onDestroy();
-    }
+        saveInSQlButton = (Button) findViewById(R.id.save);
+        saveInSQlButton.setOnClickListener(this);
+        calculateButton = (Button) findViewById(R.id.calc_button);
+        calculateButton.setOnClickListener(this);
 
-    public void saveInSQL(View view){
-        //NOP
-    }
-
-
-    public void calculateThis(View view){
         moneyET = (EditText) findViewById(R.id.eMoney);
         fuelET = (EditText) findViewById(R.id.eFuel);
         costFuelET = (EditText) findViewById(R.id.eCostFuel);
         distanseET = (EditText) findViewById(R.id.eDistanse);
         incomeTV = (TextView) findViewById(R.id.income);
         clearIncomeTV = (TextView) findViewById(R.id.clear_income);
+        onStarted();
+    }
+
+
+    protected void onStarted() {
+        super.onStart();
+        sPref = getPreferences(MODE_PRIVATE);
+        String text =  sPref.getString(EXPENSE_GASOLINE, "8.5");
+        System.out.println(text + " this loaded");
+        text = text.replace(",", ".");
+        fuelET.setText(String.format("%4.1f", Float.parseFloat(text)));
+        String costText = sPref.getString(COST_GASOLINE, "41.40");
+        costText = costText.replace(",", ".");
+        costFuelET.setText(String.format("%4.2f", Float.parseFloat(costText)));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onPause();
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sPref.edit();
+        editor.putString(EXPENSE_GASOLINE, fuelET.getText().toString().replace(",", "."));
+        editor.putString(COST_GASOLINE, costFuelET.getText().toString().replace(",", "."));
+        editor.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.calc_button: calculateThis();
+                break;
+            case R.id.save: saveInSQL();
+        }
+    }
+
+
+    public void saveInSQL(){
+        //NOP
+    }
+
+    public void calculateThis(){
+
 
         final TextView expenceOnGasoline = (TextView) findViewById(R.id.expence_gasoline);
         final TextView comission = (TextView) findViewById(R.id.commission);
 
         try {
-            aFuel = Double.parseDouble(fuelET.getText().toString());
-            aCostFuel = Double.parseDouble(costFuelET.getText().toString());
+            aFuel = Double.parseDouble(fuelET.getText().toString().replace(",", "."));
+            aCostFuel = Double.parseDouble(costFuelET.getText().toString().replace(",", "."));
         }catch (NumberFormatException e){
             aFuel = 0;
+            fuelET.setText(String.format("%4.1f", 0f));
             aCostFuel = 0;
         }
         try {
             aDistanse = Double.parseDouble(distanseET.getText().toString());
         } catch (NumberFormatException e) {
             aDistanse = 0;
+            distanseET.setText(String.format("%4.1f", 0f));
         }
 
         try {
             aMoney = Double.parseDouble(moneyET.getText().toString());
         } catch (NumberFormatException e) {
             aMoney = 0;
+            moneyET.setText(String.format("%4.2f", 0f));
         }
         double moneyOnFuel = (aFuel / 100.0 * aDistanse * aCostFuel);
         double infoStatistic = aMoney - moneyOnFuel;
